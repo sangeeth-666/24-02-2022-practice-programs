6e7d43cc1ed952d9970db87e15c545c62f9abbe6
sangeeth <ksangeeth@gmail.com>
2022-02-24 23:20:27 +0530

    "practice on signed and unsigned int"

---
+++ signedshort.c
@@ -0,0 +1,24 @@
+//signed short int
+/*#include <stdio.h>
+
+int main()
+{
+    short a=32769;
+    printf("%d",a);
+
+    return 0;
+}*/
+
+//unsigned short
+
+#include <stdio.h>
+
+int main()
+{
+    unsigned short a=65539;
+    printf("%u",a);
+    printf("%d",a);
+
+    return 0;
+}
+
