3608c4f53581b432e0022939ba3d3b74a89beae7
sangeeth <ksangeeth@gmail.com>
2022-02-24 23:22:24 +0530

    "to print a ascii value"

---
+++ asciivalue.c
@@ -0,0 +1,22 @@
+//ASCII value printing
+/*#include <stdio.h>
+
+int main()
+{
+    char ch='#';
+    printf("%d",ch);
+
+    return 0;
+}*/
+
+//singed  & unsigned char
+
+#include<stdio.h>
+
+int main()
+{
+    char c=258;
+    printf("%d",c);
+    return 0;
+}
+
